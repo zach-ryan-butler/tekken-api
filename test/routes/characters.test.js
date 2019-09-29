86453244f99c0da9e867218fddf951e267d43d89
Zach Butler <zach.standley93@gmail.com>
2019-09-29 14:47:11 -0700

    fixed my get route to search for a query should one be provided or default to fetching all the characters

--- test/routes/characters.test.js
+++ test/routes/characters.test.js
@@ -13,12 +13,29 @@ describe('character routes', () => {
   });
 
   beforeEach(() => {
-    seedData();
+    return mongoose.connection.dropDatabase()
+      .then(seedData());
   });
 
   afterAll(() => {
     return mongoose.connection.dropDatabase()
       .then(() => mongoose.connection.close());
+  });
+  
+  it('can get a character by name', () => {
+    return request(app)
+      .get('/api/v1/characters?character=akuma')
+      .then(res => {
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          character: 'Akuma',
+          imageSrc: 'https://vignette.wikia.nocookie.net/tekken/images/5/5f/Tekken-7-Akuma-CG-art.jpg/revision/latest/scale-to-width-down/310?cb=20170408183224&path-prefix=en',
+          height: '178 cm (5′10″)',
+          weight: '80 kg (176 lbs)',
+          alignment: 'Neutral/Evil',
+          __v: 0
+        });
+      });
   });
 
   it('can get characters', () => {
